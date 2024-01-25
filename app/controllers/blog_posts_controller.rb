b199065bcaa44d5f6060a4ff3a9060c571fc0ee3
ITDevSuccess <raharisontsidiany@gmail.com>
2024-01-25 11:18:29 +0300

    Short code avec before_action pour eviter le DRY

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,15 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+  # ou
+  # before_action :set_blog_post, except: [:index, :new, :create]
+
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -23,11 +28,11 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(post_params)
       redirect_to @blog_post
     else
@@ -36,7 +41,7 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
+    # @blog_post = BlogPost.find(params[:id])
     if @blog_post.destroy
       redirect_to root_path
     else
@@ -47,5 +52,9 @@ class BlogPostsController < ApplicationController
   private
   def post_params
     params.require(:blog_post).permit('title', 'body')
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
   end
 end
